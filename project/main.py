f5a0e576f8cc45eb2948e08d98a8c83ec66b5b8e
Ricardo Castillo <itic.ricardocastillo@gmail.com>
2020-06-14 14:48:09 -0500

    [WIP] BeautifulSoup - Search by element

--- project/main.py
+++ project/main.py
@@ -10,13 +10,26 @@ from bs4 import BeautifulSoup
 URL = 'http://econpy.pythonanywhere.com/ex/001.html'
 
 if __name__ == '__main__':
-    response = requests.get(URL)
-
-    if response.status_code == 200:
-        content = response.text
+    with open('econpy.html', 'r') as file:
+        content = file.read()
 
         soup = BeautifulSoup(content, 'html.parser')
 
-        # print(soup.prettify())
-        print(soup.head)
-        print(type(soup.head))
+        title = soup.find('title')
+        print(title.name)
+        print(title.text)
+        print(title.getText())
+
+
+
+# if __name__ == '__main__':
+#     response = requests.get(URL)
+#
+#     if response.status_code == 200:
+#         content = response.text
+#
+#         soup = BeautifulSoup(content, 'html.parser')
+#
+#         # print(soup.prettify())
+#         print(soup.head)
+#         print(type(soup.head.title))
