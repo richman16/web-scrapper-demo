6662e8e7591974fd0988254323a76c9d8e589743
Ricardo Castillo <itic.ricardocastillo@gmail.com>
2020-06-13 23:09:01 -0500

    [WIP] Regular expresion feature

--- project/main.py
+++ project/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Modulo para expresiones regulares
+import re
+# Modulo para manejar HTML
 import requests
 
 # URL para hacer webscrapping
@@ -12,20 +15,12 @@ if __name__ == '__main__':
     if response.status_code == 200:
         content = response.text
 
-        # Encontrar el DIV de apertura
-        regexa = '<div title="buyer-name">'
-        # Encontrar el DIV de cierre
-        regexb = '</div>'
+        # Expresion regular para encontrar el titulo
+        regex = '<div title="buyer-name">(.+?)</div>'
 
-        # Imprimir linea por linea el contenido de - content -
-        for line in content.split('\n'):
-            # Imprimir la linea donde se encuentre el DIV de apertura
-            if regexa in line:
-                # Seleccionamos la posicion donde inicia el texto
-                start = line.find(regexa) + len(regexa)
-                # Seleccionamos la posición donde termina el texto
-                end = line.find(regexb)
-                # Substring de la linea HTML
-                title = line[start:end]
-                # Imprimir el titulo
-                print(title)
+        # Primer argumento: expresion regualar
+        # Segundo argumento, STRING donde aplicar la expresion
+        # Regresa un listado de STRINGS
+        # Iteramos para poder imprimir cada uno de los elementos
+        for title in re.findall(regex, content):
+            print(title)
