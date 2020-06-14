9704ad8dd42637e8cc9ccdf5cb8af63b68cbca57
Ricardo Castillo <itic.ricardocastillo@gmail.com>
2020-06-13 22:59:30 -0500

    [WIP] Adding susbtring feature

--- project/main.py
+++ project/main.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import requests
 
+# URL para hacer webscrapping
 URL = 'http://econpy.pythonanywhere.com/ex/001.html'
 
 if __name__ == '__main__':
@@ -7,7 +11,21 @@ if __name__ == '__main__':
 
     if response.status_code == 200:
         content = response.text
-        print(content)
 
-        with open('econpy.html', 'w+') as file:
-            file.write(content)
+        # Encontrar el DIV de apertura
+        regexa = '<div title="buyer-name">'
+        # Encontrar el DIV de cierre
+        regexb = '</div>'
+
+        # Imprimir linea por linea el contenido de - content -
+        for line in content.split('\n'):
+            # Imprimir la linea donde se encuentre el DIV de apertura
+            if regexa in line:
+                # Seleccionamos la posicion donde inicia el texto
+                start = line.find(regexa) + len(regexa)
+                # Seleccionamos la posición donde termina el texto
+                end = line.find(regexb)
+                # Substring de la linea HTML
+                title = line[start:end]
+                # Imprimir el titulo
+                print(title)
